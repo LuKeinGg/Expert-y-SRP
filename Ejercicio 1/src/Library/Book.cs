ed48ec1f0a3525266018f3d4a90a24a52ff5408e
LuKeinGg <matias.alves2002@gmail.com>
2023-09-04 22:01:55 -0300

    Ejercicio 1 correjido

--- Ejercicio 1/src/Library/Book.cs
+++ Ejercicio 1/src/Library/Book.cs
@@ -5,11 +5,14 @@ namespace SRP
     public class Book
     {
 
-        public string Title { get ; }
-        public string Author { get ; }
-        public string Code { get ;  }
-        public string LibrarySector { get ; set; }
-        public string LibraryShelve { get ; set; }
+        public string Title { get; }
+        public string Author { get; }
+        public string Code { get; }
+        // public string LibrarySector { get; set; }
+        // public string LibraryShelve { get; set; }
+
+        //Creo otra clase que almacene los datos del sector y la estanteria del libro ya que la clase "Book"
+        //  no necesita conocer esos datos
 
         public Book(String title, String author, String code)
         {
@@ -18,11 +21,11 @@ namespace SRP
             this.Code = code;
         }
 
-        public void ShelveBook(String sector, String shelve)
-        {
-            this.LibrarySector = sector;
-            this.LibraryShelve = shelve;
-        }
+        // public void ShelveBook(String sector, String shelve)
+        // {
+        //     // this.LibrarySector = sector;
+        //     // this.LibraryShelve = shelve;
+        // }
 
     }
 }
